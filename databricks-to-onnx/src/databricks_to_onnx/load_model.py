67444b79fe170599752c66a0b35a9430aa0c473d
benfradet <benjamin.fradet@gmail.com>
2026-04-22 22:40:30 +0200

    function to load a model from databricks

---
+++ databricks-to-onnx/src/databricks_to_onnx/load_model.py
@@ -0,0 +1,21 @@
+import mlflow
+import torch.nn as nn
+
+
+def load_model(model_uri: str) -> nn.Module:
+    """
+    Loads a model from databricks
+    c.f. https://docs.databricks.com/aws/en/machine-learning/manage-model-lifecycle/
+
+    Parameters:
+    model_uri (str): databricks catalog model path, e.g.
+    "models:/catalog.schema.model_name@champion"
+
+    Returns:
+    a torch.nn.module ready for inference
+    """
+    mlflow.set_registry_uri("databricks-uc")
+    model = mlflow.pytorch.load_model(model_uri)  # type: ignore[reportPrivateImportUsage]
+    # evaluation mode as opposed to training mode
+    model.eval()
+    return model
