7b378a1a08c33610745ff58e1bddf79237725ca1
benfradet <benjamin.fradet@gmail.com>
2026-05-11 21:32:16 +0200

    parse id maps

--- crates/example/src/lib.rs
+++ crates/example/src/lib.rs
@@ -1,18 +1,17 @@
+use std::{collections::HashMap, fs};
+
+use anyhow::Result;
+
 pub mod model {
     include!(concat!(env!("OUT_DIR"), "/model/model.rs"));
 }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+type IdMapEntry = (i64, i64);
+type IdMap = HashMap<String, IdMapEntry>;
+
+fn parse_map(location: &str) -> Result<IdMap> {
+    let content = fs::read_to_string(location)?;
+    let id_map: IdMap = serde_json::from_str(&content)?;
+    Ok(id_map)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
