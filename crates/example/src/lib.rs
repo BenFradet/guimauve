cd9ee167cd19aec65ee7154d1914661220a4e41d
benfradet <benjamin.fradet@gmail.com>
2026-05-12 23:36:54 +0200

    placeholder plugin impl

--- crates/example/src/lib.rs
+++ crates/example/src/lib.rs
@@ -1,6 +1,7 @@
 use std::{collections::HashMap, fs};
 
 use anyhow::Result;
+use plugin::model_plugin::ModelPlugin;
 
 pub mod model {
     include!(concat!(env!("OUT_DIR"), "/model/model.rs"));
@@ -13,5 +14,26 @@ fn parse_map(location: &str) -> Result<IdMap> {
     let content = fs::read_to_string(location)?;
     let id_map: IdMap = serde_json::from_str(&content)?;
     Ok(id_map)
+}
+
+pub struct OnnxExample;
+impl ModelPlugin for OnnxExample {
+    type Error = bool;
+    type Request = bool;
+    type Response = bool;
+    type ModelInput = bool;
+    type ModelOutput = bool;
+
+    fn pre(&self, req: Self::Request) -> Result<Self::ModelInput, Self::Error> {
+        todo!()
+    }
+
+    fn infer(&self, input: Self::ModelInput) -> Result<Self::ModelOutput, Self::Error> {
+        todo!()
+    }
+
+    fn post(&self, output: Self::ModelOutput) -> Result<Self::Response, Self::Error> {
+        todo!()
+    }
 }
 
