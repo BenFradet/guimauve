7cb681ae7f70bdb0a70a3c733512df7f1f4f53ef
benfradet <benjamin.fradet@gmail.com>
2026-05-16 21:56:12 +0200

    mha init

--- python-onnx-example/mha.py
+++ python-onnx-example/mha.py
@@ -1,3 +1,47 @@
-# c.f. https://happystrongcoder.substack.com/p/autoint-automatic-feature-interaction
+import torch
+import torch.nn as nn
 
 
+class MultiHeadSelfAttention(nn.Module):
+    # c.f. https://happystrongcoder.substack.com/p/autoint-automatic-feature-interaction
+
+    def __init__(
+        self,
+        embed_size,
+        num_heads,
+        key_dim=64,
+        val_dim=None,
+        dropout=0.0,
+        use_bias=True,
+        **kwargs,
+    ) -> None:
+        super(MultiHeadSelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.num_heads = num_heads
+        self.key_dim = key_dim
+        self.val_dim = val_dim
+        self.key_output_dim = self.key_dim * num_heads
+        self.val_output_dim = (
+            self.val_dim * num_heads if val_dim else self.key_output_dim
+        )
+        self.dropout = nn.Dropout(dropout)
+        self.use_bias = use_bias
+
+        self.weight_query = nn.Parameter(
+            torch.empty((self.embed_size, self.key_output_dim))
+        )
+        self.weight_key = nn.Parameter(
+            torch.empty((self.embed_size, self.key_output_dim))
+        )
+        self.weight_value = nn.Parameter(
+            torch.empty((self.embed_size, self.val_output_dim))
+        )
+        self.weight_output = nn.Parameter(
+            torch.empty((self.val_output_dim, self.embed_size))
+        )
+
+        if use_bias:
+            self.bias_query = nn.Parameter(torch.empty(self.key_output_dim))
+            self.bias_key = nn.Parameter(torch.empty(self.key_output_dim))
+            self.bias_value = nn.Parameter(torch.empty(self.val_output_dim))
+            self.bias_output = nn.Parameter(torch.empty(self.embed_size))
