10a04d7daf9e0a040410cbfef21f561c2d97b1d4
benfradet <benjamin.fradet@gmail.com>
2026-05-21 22:33:51 +0200

    feed forward forward impl

--- python-onnx-example/transformer/feed_forward.py
+++ python-onnx-example/transformer/feed_forward.py
@@ -1,9 +1,11 @@
+import torch
 import torch.nn as nn
 
 
 class FeedForward(nn.Module):
     """
-    FFN(x) = max(0, xW1 + b1) W2 + b2 (relu)
+    sublayer: FFN(x) = max(0, xW1 + b1) W2 + b2 (relu)
+    output: LayerNorm(x + Sublayer(x))
     """
 
     def __init__(
@@ -19,3 +21,12 @@ class FeedForward(nn.Module):
         self.norm = nn.LayerNorm(self.model_dim)
         self.linear1 = nn.Linear(self.model_dim, self.feed_forward_dim)
         self.linear2 = nn.Linear(self.feed_forward_dim, self.model_dim)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        l1 = self.linear1(input).relu()
+        l2 = self.linear2(l1)
+        dropped_out = self.dropout(l2)
+        # residual connection: result is added to the input
+        summed = input + dropped_out
+        normd = self.norm(summed)
+        return normd
