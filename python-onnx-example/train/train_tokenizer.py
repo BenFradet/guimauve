32b7189b26bec60b713eb04f8b545b61e02a66b7
benfradet <benjamin.fradet@gmail.com>
2026-05-30 21:33:45 +0200

    train tokenizer on multiple input files

--- python-onnx-example/train/train_tokenizer.py
+++ python-onnx-example/train/train_tokenizer.py
@@ -3,11 +3,14 @@ import argparse
 from util.text_tokenizer import TextTokenizer
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", required=True)
+    parser = argparse.ArgumentParser(
+        prog="train_tokenizer",
+        description="Script to train a tokenizer",
+    )
+    parser.add_argument("-i", "--inputs", required=True, nargs="+")
     parser.add_argument("-o", "--output", required=True)
     args = parser.parse_args()
 
     tokenizer = TextTokenizer()
-    tokenizer.train([args.input])
+    tokenizer.train(args.inputs)
     tokenizer.save(args.output)
