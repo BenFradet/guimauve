c0d522e6b8c8eab050eac7b3b46ac1e979bb6da3
benfradet <benjamin.fradet@gmail.com>
2026-06-22 20:29:22 +0200

    onnx example uses serve fn

--- crates/onnx-example/src/main.rs
+++ crates/onnx-example/src/main.rs
@@ -1,47 +1,16 @@
-use std::{path::Path, sync::Arc};
+use std::path::Path;
 
 use anyhow::Result;
-use axum::{
-    Router,
-    extract::{self, State},
-    http::StatusCode,
-    routing::post,
-};
-use example::{TranslationPlugin, TranslationRequest, TranslationResponse};
-use plugin::model_plugin::ModelPlugin;
-use tokio::net::TcpListener;
+use example::TranslationPlugin;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let plugin = Arc::new(TranslationPlugin::new(
+    let plugin = TranslationPlugin::new(
         Path::new("../../models/pt_to_en/en_tokenizer.json"),
         Path::new("../../models/pt_to_en/pt_tokenizer.json"),
-    )?);
+    )?;
 
-    let app = Router::new()
-        .route("/infer", post(infer))
-        .with_state(plugin);
-
-    let listener = TcpListener::bind("0.0.0.0:3000").await?;
-    tracing::debug!("listening on {}", listener.local_addr()?);
-    println!("listening on {}", listener.local_addr()?);
-    let _ = axum::serve(listener, app).await;
-
-    Ok(())
-}
-
-async fn infer(
-    State(plugin): State<Arc<TranslationPlugin>>,
-    extract::Json(payload): extract::Json<TranslationRequest>,
-) -> Result<extract::Json<TranslationResponse>, (StatusCode, String)> {
-    let input = plugin.pre(payload).map_err(internal_server_error)?;
-    let output = plugin.infer(input).map_err(internal_server_error)?;
-    let response = plugin.post(output).map_err(internal_server_error)?;
-    Ok(extract::Json(response))
-}
-
-fn internal_server_error(e: anyhow::Error) -> (StatusCode, String) {
-    (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
+    plugin::server::serve(plugin, "0.0.0.0:3000").await
 }
