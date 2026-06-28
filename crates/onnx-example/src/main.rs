975bb750be950d717c42e191ebe6612000015737
benfradet <benjamin.fradet@gmail.com>
2026-06-28 20:10:28 +0200

    better tracing_sub set up

--- crates/onnx-example/src/main.rs
+++ crates/onnx-example/src/main.rs
@@ -2,10 +2,17 @@ use std::path::Path;
 
 use anyhow::Result;
 use onnx_example::TranslationPlugin;
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::registry()
+        .with(
+            EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=trace", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let plugin = TranslationPlugin::new(
         Path::new("/artifacts/en_tokenizer.json"),
