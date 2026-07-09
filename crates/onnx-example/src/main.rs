678b03a72fb50b7abb8c66452a1bace591833b34
benfradet <benjamin.fradet@gmail.com>
2026-07-09 20:42:19 +0200

    build tokio runtime manually

--- crates/onnx-example/src/main.rs
+++ crates/onnx-example/src/main.rs
@@ -2,10 +2,10 @@ use std::path::Path;
 
 use anyhow::Result;
 use onnx_example::TranslationPlugin;
+use tokio::runtime::Builder;
 use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     tracing_subscriber::registry()
         .with(
             EnvFilter::try_from_default_env()
@@ -19,5 +19,13 @@ async fn main() -> Result<()> {
         Path::new("/artifacts/pt_tokenizer.json"),
     )?;
 
-    plugin::server::serve(plugin, "0.0.0.0:3000").await
+    let cpus = std::thread::available_parallelism()?.get();
+
+    Builder::new_multi_thread()
+        .worker_threads(1)
+        // to use in conjunction with spawn_blocking
+        .max_blocking_threads(cpus.saturating_sub(1).max(1))
+        .enable_all()
+        .build()?
+        .block_on(async { plugin::server::serve(plugin, "0.0.0.0:3000").await })
 }
