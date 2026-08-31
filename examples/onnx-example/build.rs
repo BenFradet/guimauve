95e362e70aa8b13d10baea9e530e8b8112ca84a4
benfradet <benjamin.fradet@gmail.com>
2026-08-31 20:03:59 +0200

    commit onnx model for transformer

--- examples/onnx-example/build.rs
+++ examples/onnx-example/build.rs
@@ -2,7 +2,7 @@ use burn_onnx::{LoadStrategy, ModelGen};
 
 fn main() {
     ModelGen::new()
-        .input("../../models/pt_to_en/transformer.onnx")
+        .input("../../models/transformer.onnx")
         .out_dir("model/")
         .load_strategy(LoadStrategy::Embedded)
         .run_from_script();
